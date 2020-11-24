7d55d626d3079c67ae5c74a108e0c09fc226f726
edna sawe <cedna198@gmail.com>
2020-11-24 16:16:43 -0600

    new app to convert pdf to audiobook

---
+++ audio.py
@@ -0,0 +1,23 @@
+#Read a pdf document to audio
+
+import pyttsx3
+import pdfplumber
+import PyPDF2
+
+
+file = 'doc.pdf'
+
+pdfFileObj = open(file, 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+pages = pdfReader.numPages
+
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        print(text)
+        speaker = pyttsx3.init()
+        speaker.say(text)
+        speaker.runAndWait()
